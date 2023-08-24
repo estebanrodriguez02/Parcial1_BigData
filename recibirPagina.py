74c1d4e3848b774a9b12b6e1042a668708f8fbdf
estebanrodriguez02 <117477695+estebanrodriguez02@users.noreply.github.com>
2023-08-24 13:01:37 -0500

    create funcion para crear csv

---
+++ recibirPagina.py
@@ -0,0 +1,41 @@
+import boto3
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+def pagina():
+    nombre = str(datetime.today().strftime('%Y-%m-%d'))
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('parcial1bigdata')
+    obj_tiempo = bucket.Object(str("news/raw/" +
+                                   "eltiempo-" + nombre +
+                                   ".html"))
+    body_tiempo = obj_tiempo.get()['Body'].read()
+
+    html_tiempo = BeautifulSoup(body_tiempo, 'html.parser')
+    data_noticias_tiempo = html_tiempo.find_all('article')
+    csv_tiempo = ""
+    csv_publimetro = ""
+    linea_0 = "Nombre, Categoria, Link\n"
+    for i in range(len(data_noticias_tiempo)):
+        link = "eltiempo.com" + \
+               data_noticias_tiempo[i].find('a',
+                                            class_='title page-link')['href']
+        name = data_noticias_tiempo[i]['data-name'].replace(",", "")
+        category = data_noticias_tiempo[i]['data-seccion']
+        csv_tiempo += linea_0 + name + "," + \
+            category + "," + \
+            link + \
+            "\n"
+
+    boto3.client('s3').put_object(Body=csv_tiempo,
+                                  Bucket='parcial1bigdata',
+                                  Key=str('headlines/final' +
+                                          '/periodico=eltiempo/year=' +
+                                          nombre[:4]+'/month=' +
+                                          nombre[5:7]+'/day=' +
+                                          nombre[8:]+'/eltiempo.csv'))
+
+                                          
+                                          
+pagina()
