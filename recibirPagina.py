f1271e9c1d0be4b47264ba68cd880f409d268dac
SAN1713911S <112020673+SAN1713911S@users.noreply.github.com>
2023-08-29 17:32:12 +0200

    Update recibirPagina.py

--- recibirPagina.py
+++ recibirPagina.py
@@ -2,6 +2,7 @@ import boto3
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+
 def pagina():
     nombre = str(datetime.today().strftime('%Y-%m-%d'))
     s3 = boto3.resource('s3')
@@ -33,4 +34,6 @@ def pagina():
                                           nombre[:4]+'/month=' +
                                           nombre[5:7]+'/day=' +
                                           nombre[8:]+'/eltiempo.csv'))
+
+
 pagina()
