f50794b3b04f7730e3a835f52a5a22f2439b2757
SAN1713911S <112020673+SAN1713911S@users.noreply.github.com>
2023-08-29 16:39:18 +0200

    Update test_pagina.py

--- test_pagina.py
+++ test_pagina.py
@@ -1,11 +1,9 @@
 from datetime import datetime
-from leer_archivo import get_url_tiempo, get_date, get_url_publimetro
-
+from leerPagina import get_url_tiempo, get_date
 
 def test_get_date():
     dt = get_date()
     assert dt == datetime.today().strftime('%Y-%m-%d')
-
 
 def test_get_url_tiempo():
     assert get_url_tiempo() == "https://www.eltiempo.com/"
