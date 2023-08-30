c9055deb9719718411b9ca250634a7c6b8019825
estebanrodriguez02 <117477695+estebanrodriguez02@users.noreply.github.com>
2023-08-30 14:35:41 -0500

    Update recibirPagina.py

--- recibirPagina.py
+++ recibirPagina.py
@@ -1,3 +1,44 @@
+# import boto3
+# from datetime import datetime
+# from bs4 import BeautifulSoup
+
+
+# def pagina():
+#     nombre = str(datetime.today().strftime('%Y-%m-%d'))
+#     s3 = boto3.resource('s3')
+#     bucket = s3.Bucket('parcial1bigdata')
+#     obj_tiempo = bucket.Object(str("news/raw/" +
+#                                    "eltiempo-" + nombre +
+#                                    ".html"))
+#     body_tiempo = obj_tiempo.get()['Body'].read()
+
+#     html_tiempo = BeautifulSoup(body_tiempo, 'html.parser')
+#     data_noticias_tiempo = html_tiempo.find_all('article')
+#     csv_tiempo = ""
+#     linea_0 = "Nombre; Categoria; Link\n"
+#     for i in range(len(data_noticias_tiempo)):
+#         link = "eltiempo.com" + \
+#                data_noticias_tiempo[i].find('a',
+#                                             class_='title page-link')['href']
+#         name = data_noticias_tiempo[i]['data-name'].replace(",", "")
+#         category = data_noticias_tiempo[i]['data-seccion']
+#         csv_tiempo += category + ";" + \
+#             name + ";" + \
+#             link + \
+#             "\n"
+
+#     boto3.client('s3').put_object(Body=csv_tiempo,
+#                                   Bucket='parcial1bigdata',
+#                                   Key=str('headlines/final' +
+#                                           '/periodico=eltiempo/year=' +
+#                                           nombre[:4]+'-month=' +
+#                                           nombre[5:7]+'-day=' +
+#                                           nombre[8:]+'-eltiempo.csv'))
+
+
+# pagina()
+
+
 import boto3
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -15,17 +56,21 @@ def pagina():
     html_tiempo = BeautifulSoup(body_tiempo, 'html.parser')
     data_noticias_tiempo = html_tiempo.find_all('article')
     csv_tiempo = ""
-    # linea_0 = "Nombre; Categoria; Link\n"
+    linea_0 = "Nombre; Categoria; Link\n"
+
     for i in range(len(data_noticias_tiempo)):
         link = "eltiempo.com" + \
                data_noticias_tiempo[i].find('a',
                                             class_='title page-link')['href']
         name = data_noticias_tiempo[i]['data-name'].replace(",", "")
         category = data_noticias_tiempo[i]['data-seccion']
-        csv_tiempo += name + ";" + \
-            category + ";" + \
+        csv_tiempo += category + ";" + \
+            name + ";" + \
             link + \
             "\n"
+
+    if not csv_tiempo.startswith(linea_0):
+        csv_tiempo = linea_0 + csv_tiempo
 
     boto3.client('s3').put_object(Body=csv_tiempo,
                                   Bucket='parcial1bigdata',
@@ -37,3 +82,4 @@ def pagina():
 
 
 pagina()
+
