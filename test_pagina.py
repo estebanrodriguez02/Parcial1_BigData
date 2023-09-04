4b0198cb47a9c97db5e2b59b6eeeb24ba1db288b
SAN1713911S <112020673+SAN1713911S@users.noreply.github.com>
2023-09-04 14:23:18 -0500

    Update test_pagina.py

--- test_pagina.py
+++ test_pagina.py
@@ -1,17 +1,17 @@
-from datetime import datetime
 from recibirPagina import get_titularNoticias
 from leerPagina import get_url_tiempo
 import requests
 from bs4 import BeautifulSoup
 
 def test_get_titularNoticias():
+    
     get = get_titularNoticias()
     url = "https://www.eltiempo.com/"
     # Realizar una solicitud GET a la página
     response = requests.get(url)
     # Parsear el contenido HTML de la página
     soup = BeautifulSoup(response.text, 'html.parser')
-    # Encontrar el primer titular 
+    # Encontrar el primer titular
     primer_titular = soup.find('a', class_='title page-link').text
     assert get == primer_titular
 
