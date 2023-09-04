5376c4e6ea17a27bbe7061d9f56874a7492c7a85
SAN1713911S <112020673+SAN1713911S@users.noreply.github.com>
2023-09-04 14:09:46 -0500

    Update recibirPagina.py

--- recibirPagina.py
+++ recibirPagina.py
@@ -36,9 +36,10 @@ def pagina():
                                           nombre[:4]+'-month=' +
                                           nombre[5:7]+'-day=' +
                                           nombre[8:]+'-eltiempo.csv'))
-    
+
 pagina()
-   
+
 def get_titularNoticias():
+    
     global primer_titular  # Acceder a la variable global primer_titular
     return primer_titular
