9abf56fc86481cea8e6a8462aa3343bf920c98e9
estebanrodriguez02 <117477695+estebanrodriguez02@users.noreply.github.com>
2023-09-04 14:48:43 -0500

    Update test_pagina.py

--- test_pagina.py
+++ test_pagina.py
@@ -20,11 +20,14 @@ def test_contenedoresHTML():
     # Realizar una solicitud GET a la página "eltiempo.com"
     url = get_url_tiempo()
     response = requests.get(url)
-    self.html_tiempo = BeautifulSoup(response.text, 'html.parser')
-    a_tags = self.html_tiempo.find_all('a')
-    self.assertTrue(len(a_tags) > 0, "No se encontraron etiquetas <a> en la página")
-    h1_tags = self.html_tiempo.find_all('h1')
-    self.assertTrue(len(h1_tags) > 0, "No se encontraron etiquetas <h1> en la página")
-    p_tags = self.html_tiempo.find_all('p')
-    self.assertTrue(len(p_tags) > 0, "No se encontraron etiquetas <p> en la página")
+    html_tiempo = BeautifulSoup(response.text, 'html.parser')
+    # Verificar si hay al menos una etiqueta <a> en la página
+    a_tags = html_tiempo.find_all('a')
+    assert len(a_tags) > 0, "No se encontraron etiquetas <a> en la página"
+    # Verificar si hay al menos una etiqueta <h1> en la página
+    h1_tags = html_tiempo.find_all('h1')
+    assert len(h1_tags) > 0, "No se encontraron etiquetas <h1> en la página"
+    # Verificar si hay al menos una etiqueta <p> en la página
+    p_tags = html_tiempo.find_all('p')
+    assert len(p_tags) > 0, "No se encontraron etiquetas <p> en la página"
     
